ad9cae12a1c0d13e602d319368a90a175e0e4274
suragch <suragch@suragch.dev>
2024-11-16 09:33:47 +0800

    update site

---
+++ assets/js/5e95c892.4a99c1bf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjacksresearch=self.webpackChunkjacksresearch||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>k});r(6540);var a=r(4164),c=r(1003),u=r(7559),h=r(2831),n=r(781),t=r(4848);function k(e){return(0,t.jsx)(c.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,h.v)(e.route.routes)})})}}}]);
--- assets/js/78e949da.63622adf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkjacksresearch=self.webpackChunkjacksresearch||[]).push([[877],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
