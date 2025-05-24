e50e0b99c5286539b80d34f850fe9ba2ae5d205d
suragch <suragch@suragch.dev>
2025-05-24 10:29:19 +0800

    update with app link images

---
+++ assets/js/5e95c892.46b190b5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjacksresearch=self.webpackChunkjacksresearch||[]).push([[647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>k});r(6540);var a=r(4164),c=r(1769),u=r(204),h=r(2831),n=r(1410),t=r(4848);function k(e){return(0,t.jsx)(c.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,h.v)(e.route.routes)})})}}}]);
